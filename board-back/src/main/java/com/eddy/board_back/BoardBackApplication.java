96521744a825c67843decf6665b412a2955fd385
eddyhwang97 <hwangdw1997@gmail.com>
2025-04-11 15:45:30 +0900

    Update BoardBackApplication.java

--- board-back/src/main/java/com/eddy/board_back/BoardBackApplication.java
+++ board-back/src/main/java/com/eddy/board_back/BoardBackApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BoardBackApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BoardBackApplication.class, args);
 	}
